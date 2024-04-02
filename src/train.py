fa67795fbfddb4524b2b37c8a9d9ddbe61dfdbf2
Max Huber <55286559+MaxHuber888@users.noreply.github.com>
2024-04-01 22:30:13 -0400

    Added train checkpoints, new training notebook

--- src/train.py
+++ src/train.py
@@ -1,9 +1,12 @@
+import os
+
 from keras.optimizers import Adam, SGD
-import pickle
+import pandas as pd
+from keras.callbacks import ModelCheckpoint
 from keras.models import load_model
 
 
-def prep_and_train_model(model, optim, train_data, test_data, epochs=1, batch_size=10):
+def prep_and_train_model(model, optim, train_data, test_data, epochs=1, batch_size=10, model_name="temp"):
     # Initialize optimizer
     if optim == "adam":
         optimizer = Adam()
@@ -15,8 +18,14 @@ def prep_and_train_model(model, optim, train_data, test_data, epochs=1, batch_si
 
     # Compile the model for training
     model.compile(loss="binary_crossentropy", optimizer=optimizer, metrics=["accuracy"])
+    # loss="sparse_categorical_crossentropy"
 
-    # TODO: Add parallel workers
+    # Saves the model whenever a new max val accuracy is reached
+    model_checkpoint_callback = ModelCheckpoint(
+        filepath=f"saved_models/{model_name}.keras",
+        monitor='val_accuracy',
+        mode='max',
+        save_best_only=True)
 
     # Train the model
     history = model.fit(
@@ -24,26 +33,23 @@ def prep_and_train_model(model, optim, train_data, test_data, epochs=1, batch_si
         batch_size=batch_size,
         epochs=epochs,
         validation_data=test_data,
-        workers=4,
-        use_multiprocessing=True,
         shuffle=True,
+        callbacks=[model_checkpoint_callback]
     )
 
     return history, model
 
 
-def save_model(history, model, out_path):
-    model.save(out_path)
-    history_path = out_path + "_history.pkl"
-    with open(history_path, "w") as f:
-        json.dump(history.history, f)
-
-    print(
-        f"Model and training history are saved at {out_path} and {history_path} respectively."
-    )
+def save_history(history, model_name):
+    hist_df = pd.DataFrame(history.history)
+    # If previous history exists, concatenate histories
+    if os.path.exists("saved_models/" + model_name + "_history.csv"):
+        previous_hist_df = pd.read_csv("saved_models/" + model_name + "_history.csv")
+        hist_df = pd.concat([previous_hist_df, hist_df], axis=0, ignore_index=True)
+    hist_df.to_csv("saved_models/" + model_name + "_history.csv")
 
 
-def load_model(model_path):
-    model = load_model(model_path)
+def load_model_from_path(model_path):
+
     print(f"Model loaded from {model_path}.")
     return model
